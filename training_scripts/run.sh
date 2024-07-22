a249aef9a3ea1761a3ffa2b4cc7b59fd365019b6
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-07-23 01:41:54 +0530

    better_optimizers_unet6

--- training_scripts/run.sh
+++ training_scripts/run.sh
@@ -1,6 +1,6 @@
 export SUBJECT="pickup truck"
 export FILE_ID="template_truck"
-export RUN_NAME="better_optimizers"  
+export RUN_NAME="better_optimizers_unet6"  
 
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
 export INSTANCE_DIR="../training_data_vaibhav/ref_imgs_multiobject" 
@@ -24,12 +24,12 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --output_dir=$OUTPUT_DIR \
   --vis_dir=$VIS_DIR \
   --instance_prompt="Continuous MLP Training" \
-  --train_text_encoder \
   --train_unet \
   --textual_inv \
   --resolution=512 \
   --train_batch_size=2 \
   --inference_batch_size=4 \
+  --lora_rank=6 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --learning_rate_text=5e-5 \
