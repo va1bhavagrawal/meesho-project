6d5ee5ea0dd398710bc29d6eaf0dc116eefd26d8
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-07-27 08:20:52 +0530

    newtruck_notext

--- training_scripts/run.sh
+++ training_scripts/run.sh
@@ -1,6 +1,6 @@
 export SUBJECT="pickup truck"
 export FILE_ID="template_truck"
-export RUN_NAME="mergedtoken_text"   
+export RUN_NAME="newtruck_notext"   
 
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
 export INSTANCE_DIR="../training_data_vaibhav/ref_imgs_multiobject" 
@@ -28,8 +28,8 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --train_text_encoder \
   --textual_inv \
   --resolution=512 \
-  --train_batch_size=2 \
-  --inference_batch_size=2 \
+  --train_batch_size=1 \
+  --inference_batch_size=4 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --learning_rate_text=5e-5 \
