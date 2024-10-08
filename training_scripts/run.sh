febef4a6a167cd57a78a10e4848ffec64666b219
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-10-09 02:46:34 +0530

    bboxattnloss0.01

--- training_scripts/run.sh
+++ training_scripts/run.sh
@@ -1,4 +1,4 @@
-export RUN_NAME="bboxattnloss0.01_lr1e-5" 
+export RUN_NAME="bboxattnloss0.01" 
 # export RUN_NAME="debug" 
 
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
@@ -18,7 +18,7 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --train_text_encoder="N" \
   --use_controlnet_images="Y" \
   --use_ref_images="Y" \
-  --learning_rate=1e-5 \
+  --learning_rate=1e-4 \
   --learning_rate_mlp=1e-3 \
   --learning_rate_merger=1e-4 \
   --learning_rate_emb=1e-3 \
