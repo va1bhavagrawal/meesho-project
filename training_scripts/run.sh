fb3fafd92f993f140cc132df91a2fe9bfca43fc1
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-10-13 21:27:39 +0530

    noreplacement_withpriorpreservation_safer

--- training_scripts/run.sh
+++ training_scripts/run.sh
@@ -1,4 +1,4 @@
-export RUN_NAME="noreplacement_withpriorpreservation"  
+export RUN_NAME="noreplacement_withpriorpreservation_safer"  
 # export RUN_NAME="debug" 
 
 export MODEL_NAME="stabilityai/stable-diffusion-2-1"
@@ -41,7 +41,7 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --train_batch_size=1 \
   --inference_batch_size=4 \
   --use_location_conditioning="N" \
-  --attn_bbox_from_class_mean="Y" \
+  --attn_bbox_from_class_mean="N" \
   --prior_loss_weight=0.1 \
   --special_token_attn_loss_weight=0.1 \
   --gradient_accumulation_steps=1 \
