e243393f3c7b11f5eb79f835e6cc7fabf69cdfa6
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-10-24 18:20:35 +0530

    correction, increased the number of steps to account for increased effective bs

--- training_scripts/run.sh
+++ training_scripts/run.sh
@@ -37,8 +37,8 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --with_prior_preservation="Y" \
   --root_data_dir=$ROOT_DATA_DIR \
   --controlnet_prompts_file=$CONTROLNET_PROMPTS_FILE \
-  --stage1_steps=5000 \
-  --stage2_steps=95000 \
+  --stage1_steps=10000 \
+  --stage2_steps=190000 \
   --resolution=512 \
   --train_batch_size=2 \
   --inference_batch_size=4 \
