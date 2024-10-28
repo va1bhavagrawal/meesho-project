d5936dc054a50946e9d7dc18be29a483b4e6bb49
Vaibhav Agrawal <agrvaibhav19@gmail.com>
2024-10-28 07:11:37 +0530

    major potential correction, reduced num_steps attribute in the dataset

--- training_scripts/train.py
+++ training_scripts/train.py
@@ -1228,7 +1228,7 @@ def main(args):
         tokenizer=tokenizer, 
         ref_imgs_dirs=[args.instance_data_dir_1subject], 
         controlnet_imgs_dirs=[args.controlnet_data_dir_1subject], 
-        num_steps=args.stage1_steps, 
+        num_steps=args.stage1_steps // accelerator.num_processes + 1, 
         gpu_idx=accelerator.process_index, 
     ) 
 
@@ -1237,7 +1237,7 @@ def main(args):
         tokenizer=tokenizer, 
         ref_imgs_dirs=[args.instance_data_dir_1subject, args.instance_data_dir_2subjects],  
         controlnet_imgs_dirs=[args.controlnet_data_dir_1subject, args.controlnet_data_dir_2subjects],  
-        num_steps=args.stage2_steps, 
+        num_steps=args.stage2_steps // accelerator.num_processes + 1,  
         gpu_idx=accelerator.process_index, 
     ) 
 
